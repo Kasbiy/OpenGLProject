fd2775d5debbc8440aa41bf8c18a2ea0fb5d3185
Charles Polart <charles.polart@hotmail.fr>
2022-02-10 11:51:15 +0100

    Compute normals

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -21,125 +21,31 @@ BEGIN_VISUALIZER_NAMESPACE
 struct VertexDataPosition3fColor3f
 {
     glm::vec3 position;
+    glm::vec3 normal;
     glm::vec3 color;
 };
 
-void GenerateSphereMesh(std::vector<VertexDataPosition3fColor3f>& vertices, std::vector<uint16_t>& indices, uint16_t sphereStackCount, uint16_t sphereSectorCount, glm::vec3 sphereCenter, float sphereRadius)
+float computeMagnitude(const glm::vec3 &v)
 {
-    std::size_t vertexId = 0;
+    return (std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2) + std::pow(v.z, 2)));
+}
 
-    for (uint16_t i = 0; i <= sphereStackCount; ++i)
+glm::vec3 computeNormal(const glm::vec3 &A, const glm::vec3 &B, const glm::vec3 &C)
+{
+    glm::vec3 N = glm::cross(A - B, B - C);
+
+    if (computeMagnitude(N) > 0)
     {
-        const float t = static_cast<float>(i) / static_cast<float>(sphereStackCount);
-
-        const float stackAngle = glm::pi<float>() / 2.0f - t * glm::pi<float>();
-
-        const float nxz = glm::cos(stackAngle);
-        const float ny = glm::sin(stackAngle);
-
-        const float xz = sphereRadius * nxz;
-        const float y = sphereRadius * ny;
-
-        const float mul = stackAngle < 0.0f ? -1.0f : 1.0f;
-
-        const float dxz = -ny * mul;
-
-        // When stackAngle is null dxz is equal to 0 and it is impossible to compute the tangents and bitangents
-        // So we use the up vector as a bitangent
-        const bool useUpVector = (sphereStackCount % 2 == 0) && (i == sphereStackCount / 2);
-
-        for (uint16_t j = 0; j <= sphereSectorCount; ++j)
-        {
-            const float s = static_cast<float>(j) / static_cast<float>(sphereSectorCount);
-
-            const float sectorAngle = s * 2.0f * glm::pi<float>();
-
-            const float csa = glm::cos(sectorAngle);
-            const float ssa = glm::sin(sectorAngle);
-
-            const float nx = nxz * csa;
-            const float nz = nxz * ssa;
-
-            const float x = xz * csa;
-            const float z = xz * ssa;
-
-            const glm::vec3 direction = glm::vec3(x, y, z);
-
-            const glm::vec3 position = sphereCenter + direction;
-
-            const glm::vec2 texcoords = glm::vec2(s, t);
-
-            const glm::vec3 normal = glm::vec3(nx, ny, nz);
-
-            const glm::vec3 color = glm::vec3(0.5f) + 0.5f * glm::normalize(direction);
-
-            vertices[vertexId++] = { position, color };
-
-            if (useUpVector)
-            {
-                const glm::vec3 bitangent = glm::vec3(0.0f, 0.0f, 1.0f);
-                const glm::vec3 tangent = glm::cross(normal, bitangent);
-            }
-            else
-            {
-                const glm::vec3 tangent = glm::normalize(glm::vec3(dxz * -glm::sin(sectorAngle), dxz * glm::cos(sectorAngle), 0.0f));
-                const glm::vec3 bitangent = glm::cross(tangent, normal);
-            }
-        }
+        return (glm::normalize(N));
     }
-
-    const uint16_t sectorCountplusOne = sphereSectorCount + 1;
-
-    std::size_t indexID = 0;
-
-    for (uint16_t j = 0; j < sphereSectorCount; ++j)
-    {
-        const uint16_t jp1 = j + 1;
-
-        indices[indexID++] = j;
-        indices[indexID++] = sectorCountplusOne + j;
-        indices[indexID++] = sectorCountplusOne + jp1;
-    }
-
-    for (uint16_t i = 1; i < sphereStackCount - 1; ++i)
-    {
-        const uint16_t k1 = i * sectorCountplusOne;
-        const uint16_t k2 = k1 + sectorCountplusOne;
-
-        for (uint16_t j = 0; j < sphereSectorCount; ++j)
-        {
-            const uint16_t jp1 = j + 1;
-
-            indices[indexID++] = k1 + j;
-            indices[indexID++] = k2 + j;
-            indices[indexID++] = k1 + jp1;
-
-            indices[indexID++] = k1 + jp1;
-            indices[indexID++] = k2 + j;
-            indices[indexID++] = k2 + jp1;
-        }
-    }
-
-    const uint16_t k1 = (sphereStackCount - 1) * sectorCountplusOne;
-    const uint16_t k2 = k1 + sectorCountplusOne;
-
-    for (uint16_t j = 0; j < sphereSectorCount; ++j)
-    {
-        const uint16_t jp1 = j + 1;
-
-        indices[indexID++] = k1 + j;
-        indices[indexID++] = k2 + j;
-        indices[indexID++] = k1 + jp1;
-    }
+    return (glm::vec3());
 }
 
 void LoadSceneMesh(std::vector<VertexDataPosition3fColor3f> &vertices, std::vector<uint32_t> &indices)
 {
     tinyobj::ObjReader reader;
-    std::string inputfile = "C:/Users/Charles Polart/Documents/EpitechOpenGL/OpenGLProject/obj/dragon.obj";
+    std::string inputfile = "path_to_obj";
     tinyobj::ObjReaderConfig reader_config;
-
-    reader_config.mtl_search_path = "C:/Users/Charles Polart/Documents/EpitechOpenGL/OpenGLProject/obj/";
 
     if (!reader.ParseFromFile(inputfile, reader_config))
     {
@@ -166,6 +72,11 @@ void LoadSceneMesh(std::vector<VertexDataPosition3fColor3f> &vertices, std::vect
         {
             size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
 
+            if (fv != 3)
+            {
+                std::cerr << "Error: is not a triangle" << std::endl;
+                exit(1);
+            }
             for (size_t v = 0; v < fv; ++v)
             {
                 tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
@@ -176,6 +87,13 @@ void LoadSceneMesh(std::vector<VertexDataPosition3fColor3f> &vertices, std::vect
                 glm::vec3 color(0.8, 0.8, 0.8);
                 vertices.push_back(visualizer::VertexDataPosition3fColor3f{position, color});
             }
+            visualizer::VertexDataPosition3fColor3f &A = vertices[vertices.size() - 3];
+            visualizer::VertexDataPosition3fColor3f &B = vertices[vertices.size() - 2];
+            visualizer::VertexDataPosition3fColor3f &C = vertices[vertices.size() - 1];
+            glm::vec3 normal = computeNormal(A.position, B.position, C.position);
+            A.normal = normal;
+            B.normal = normal;
+            C.normal = normal;
             index_offset += fv;
             indices_size += fv;
         }
@@ -200,7 +118,7 @@ void Renderer::Initialize()
 
     glCreateBuffers(1, &m_UBO);
     glNamedBufferStorage(m_UBO, sizeof(glm::mat4), glm::value_ptr(m_Camera->GetViewProjectionMatrix()), GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT);
-    m_UBOData = reinterpret_cast<glm::mat4*>(glMapNamedBufferRange(m_UBO, 0, sizeof(glm::mat4), GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_FLUSH_EXPLICIT_BIT));
+    m_UBOData = reinterpret_cast<glm::mat4 *>(glMapNamedBufferRange(m_UBO, 0, sizeof(glm::mat4), GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_FLUSH_EXPLICIT_BIT));
 
     glCreateBuffers(1, &m_VBO);
     glNamedBufferStorage(m_VBO, sizeof(VertexDataPosition3fColor3f) * vertexCount, vertices.data(), 0);
@@ -217,7 +135,9 @@ void Renderer::Initialize()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexDataPosition3fColor3f), nullptr);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexDataPosition3fColor3f), reinterpret_cast<GLvoid*>(sizeof(glm::vec3)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexDataPosition3fColor3f), reinterpret_cast<GLvoid *>(sizeof(glm::vec3)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(VertexDataPosition3fColor3f), reinterpret_cast<GLvoid *>(sizeof(glm::vec3)));
 
     glBindVertexArray(0);
 
@@ -226,6 +146,7 @@ void Renderer::Initialize()
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(2);
 
     GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
     GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -236,7 +157,7 @@ void Renderer::Initialize()
     glAttachShader(m_ShaderProgram, fShader);
 
     {
-        char const* const vertexShader =
+        char const *const vertexShader =
             R"(#version 450 core
 
 layout(location = 0) in vec3 inWorldPos;
@@ -271,11 +192,12 @@ void main()
 
                 glGetShaderInfoLog(vShader, length, nullptr, log.data());
 
-                std::cerr << "Vertex shader log:\n" << log << '\n';
+                std::cerr << "Vertex shader log:\n"
+                          << log << '\n';
             }
         }
 
-        char const* const fragmentShader =
+        char const *const fragmentShader =
             R"(#version 450 core
 
 layout(location = 0) out vec4 outColor;
@@ -303,7 +225,8 @@ void main()
 
                 glGetShaderInfoLog(fShader, length, nullptr, log.data());
 
-                std::cerr << "Vertex shader log:\n" << log << '\n';
+                std::cerr << "Vertex shader log:\n"
+                          << log << '\n';
             }
         }
     }
@@ -321,7 +244,8 @@ void main()
 
             glGetProgramInfoLog(m_ShaderProgram, length, nullptr, log.data());
 
-            std::cerr << "Shader program log:\n" << log << '\n';
+            std::cerr << "Shader program log:\n"
+                      << log << '\n';
         }
     }
 
